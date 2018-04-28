30162db90e5f9801abe8ce58463ce59c424f83c0
brettCole <acodercalledcole@gmail.com>
2018-04-28 17:26:24 -0400

    added routes to app.js and forms

--- client/src/index.js
+++ client/src/index.js
@@ -5,5 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+);
 registerServiceWorker();
