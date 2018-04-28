8a0486e20eb1dc5df8d755f238c373777f3172fd
brettCole <acodercalledcole@gmail.com>
2018-04-28 18:07:06 -0400

    redux store and localstorage

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './store';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-  <App />, 
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
--- client/src/reducers/searchResults.js
+++ client/src/reducers/searchResults.js
@@ -7,4 +7,4 @@ export default function searchResults(state = [], action) {
     default:
       return state;
   }
-} 
+}   
